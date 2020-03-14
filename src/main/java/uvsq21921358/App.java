35a0fdaf4e5dd2a86af2e5490cc9087732016c8a
SIMPORE <nayimasimpore@gmail.com>
2020-03-14 13:21:16 +0100

    Implémentation du pattern COMMAND

---
+++ src/main/java/uvsq21921358/App.java
@@ -0,0 +1,13 @@
+package uvsq21921358;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
